d191113e1ea846cdb8a8ffaad37b5dababbac2c4
Michael Luthor <michael.the.ranidae@gmail.com>
2014-09-22 17:56:10 +0800

    Add demo index views.

--- Module/Lunome/Action/Index.php
+++ Module/Lunome/Action/Index.php
@@ -5,15 +5,36 @@
 namespace X\Module\Lunome\Action;
 
 /**
+ * Use statements
+ */
+use X\Util\Action\Visual;
+use X\Service\XView\Core\Handler\Html;
+
+/**
  * The action class for index action.
  * @author Unknown
  */
-class Index extends \X\Service\XAction\Core\Handler\WebAction { 
+class Index extends Visual { 
     /** 
      * The action handle for index action.
      * @return void
      */ 
     public function runAction( /* @TODO Add parameters here if you need. */ ) {
-        /* @TODO Input your own code here. */
+        /* Load layout. */
+        $this->getView()->loadLayout(Html::LAYOUT_SINGLE_COLUMN_FULL_WIDTH);
+        
+        /* Load navigation bar */
+        $name   = 'INDEX_NAV_BAR';
+        $path   = $this->getParticleViewPath('Util/Navigation');
+        $option = array('zone'=>'header');
+        $data   = array();
+        $this->getView()->loadParticle($name, $path, $option, $data);
+        
+        /* Load movie particle view. */
+        $name   = 'INDEX_MOVIE';
+        $path   = $this->getParticleViewPath('Index/Movie');
+        $option = array();
+        $data   = array();
+        $this->getView()->loadParticle($name, $path, $option, $data);
     }
 }
