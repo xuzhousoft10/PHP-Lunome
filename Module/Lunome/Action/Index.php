d3dd43ad347915ebc696d059e02d32fe40e4fb52
Michael Luthor <michael.the.ranidae@gmail.com>
2014-09-22 17:19:43 +0800

    Add index action for lunome module.

---
+++ Module/Lunome/Action/Index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The action file for index action.
+ */
+namespace X\Module\Lunome\Action;
+
+/**
+ * The action class for index action.
+ * @author Unknown
+ */
+class Index extends \X\Service\XAction\Core\Handler\WebAction { 
+    /** 
+     * The action handle for index action.
+     * @return void
+     */ 
+    public function runAction( /* @TODO Add parameters here if you need. */ ) {
+        /* @TODO Input your own code here. */
+    }
+}
