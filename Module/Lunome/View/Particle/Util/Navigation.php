9c22ef61d7d9870e4f31776d960e05f4598cab93
michael <michael.the.ranidae@gmail.com>
2014-09-22 21:01:34 +0800

    Fixex the notice of Navigation.php.

--- Module/Lunome/View/Particle/Util/Navigation.php
+++ Module/Lunome/View/Particle/Util/Navigation.php
@@ -22,7 +22,7 @@ $this->addStyle('body', array('padding-top'=>'70px'));
         <li><a href="#">Link</a></li>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
+          <ul class="dropdown-menu">
             <li><a href="#">Action</a></li>
             <li><a href="#">Another action</a></li>
             <li><a href="#">Something else here</a></li>
@@ -33,7 +33,7 @@ $this->addStyle('body', array('padding-top'=>'70px'));
           </ul>
         </li>
       </ul>
-      <form class="navbar-form navbar-left" role="search">
+      <form class="navbar-form navbar-left">
         <div class="form-group">
           <input type="text" class="form-control" placeholder="Search">
         </div>
@@ -43,7 +43,7 @@ $this->addStyle('body', array('padding-top'=>'70px'));
         <li><a href="#">Link</a></li>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
+          <ul class="dropdown-menu">
             <li><a href="#">Action</a></li>
             <li><a href="#">Another action</a></li>
             <li><a href="#">Something else here</a></li>
