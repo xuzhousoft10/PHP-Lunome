e3b6dea805e271ff6b0366e63ac21ec1f52dd867
Michael Luthor <michael.the.ranidae@gmail.com>
2014-09-22 15:13:24 +0800

    Add action file for user register.

---
+++ Module/Lunome/Action/User/Register.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The action file to handle register.
+ */
+namespace X\Module\Lunome\Action\User;
+
+/**
+ * The action class for register action.
+ * 
+ * @author Michael Luthor <michaelluthor@163.com>
+ */
+class Register extends \X\Service\XAction\Core\Handler\WebAction { 
+    /**
+     * Execute the registion.
+     * 
+     * @return void
+     */
+    public function runAction( $email, $password ) {
+        echo "User login action has not been implemented.";
+    }
+}
