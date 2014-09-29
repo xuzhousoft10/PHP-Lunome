a0e54ae2c73bcc09a21c58d5b3c0eb8c5c700dea
Michael Luthor <michael.the.ranidae@gmail.com>
2014-09-29 18:00:06 +0800

    Add the migraion 'create_db_table_accounts' for lunome module.

---
+++ Module/Lunome/Migration/M00000_create_db_table_accounts.php
@@ -0,0 +1,37 @@
+<?
+/** 
+ * Migration file for create_db_table_accounts 
+ */
+namespace X\Module\Lunome\Migration;
+
+/**
+ * Use statements
+ */
+use X\Service\XDatabase\Core\Table\Manager as TableManager;
+use X\Service\XDatabase\Core\Table\Column;
+
+/** 
+ * M00000_create_db_table_accounts 
+ */
+class M00000_create_db_table_accounts extends \X\Core\Module\Migrate {
+    /** 
+     * (non-PHPdoc)
+     * @see \X\Core\Module\InterfaceMigrate::up()
+     */
+    public function up() {
+        $columns = array();
+        $columns[] = Column::create('id')->varchar(13)->notNull();
+        $columns[] = Column::create('account')->int()->notNull();
+        $columns[] = Column::create('created_at')->datetime()->notNull();
+        $columns[] = Column::create('status')->int(Column::T_TINYINT)->notNull()->defaultVal(0);
+        TableManager::create('accounts', $columns);
+    }
+
+    /** 
+     * (non-PHPdoc)
+     * @see \X\Core\Module\InterfaceMigrate::down()
+     */
+    public function down() {
+        TableManager::open('accounts')->drop();
+    }
+}
