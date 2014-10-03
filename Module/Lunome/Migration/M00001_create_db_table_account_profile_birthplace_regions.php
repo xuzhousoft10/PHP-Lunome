f6012d5a9ce5756456ce5af0d3a8a522e797ac87
michael <michael.the.ranidae@gmail.com>
2014-10-03 16:31:54 +0800

    [module][lunome]Update migrations.

---
+++ Module/Lunome/Migration/M00001_create_db_table_account_profile_birthplace_regions.php
@@ -0,0 +1,39 @@
+<?php
+/** 
+ * Migration file for create_db_table_account_profile_birthplace_regions 
+ */
+namespace X\Module\Lunome\Migration;
+
+/**
+ * Use statements
+ */
+use X\Service\XDatabase\Core\Table\Column;
+use X\Service\XDatabase\Core\Table\ColumnType;
+use X\Service\XDatabase\Core\Table\Manager as TableManager;
+
+/** 
+ * M00001_create_db_table_account_profile_birthplace_regions 
+ */
+class M00001_create_db_table_account_profile_birthplace_regions extends \X\Core\Module\Migrate {
+    /** 
+     * (non-PHPdoc)
+     * @see \X\Core\Module\InterfaceMigrate::up()
+     */
+    public function up() {
+        $columns = array();
+        $columns[] = Column::create('id')->setType(ColumnType::T_INT)->setIsUnsigned(true)->setNullable(false)->setIsAutoIncrement(true);
+        $columns[] = Column::create('account_id')->setType(ColumnType::T_INT)->setIsUnsigned(true)->setNullable(false);
+        $columns[] = Column::create('region_level')->setType(ColumnType::T_TINYINT)->setIsUnsigned(true)->setNullable(false);
+        $columns[] = Column::create('region_id')->setType(ColumnType::T_INT)->setIsUnsigned(true)->setNullable(false);
+        $table = TableManager::create('account_profile_birthplace_regions', $columns, 'id');
+        $table->addUnique('id');
+    }
+
+    /** 
+     * (non-PHPdoc)
+     * @see \X\Core\Module\InterfaceMigrate::down()
+     */
+    public function down() {
+        TableManager::open('account_profile_birthplace_regions')->drop();
+    }
+}
---
+++ Module/Lunome/Migration/M00004_create_db_table_account_profile_contact_place_regions.php
@@ -0,0 +1,39 @@
+<?php
+/** 
+ * Migration file for create_db_table_account_profile_contact_place_regions 
+ */
+namespace X\Module\Lunome\Migration;
+
+/**
+ * Use statements
+ */
+use X\Service\XDatabase\Core\Table\Column;
+use X\Service\XDatabase\Core\Table\ColumnType;
+use X\Service\XDatabase\Core\Table\Manager as TableManager;
+
+/** 
+ * M00004_create_db_table_account_profile_contact_place_regions 
+ */
+class M00004_create_db_table_account_profile_contact_place_regions extends \X\Core\Module\Migrate {
+    /** 
+     * (non-PHPdoc)
+     * @see \X\Core\Module\InterfaceMigrate::up()
+     */
+    public function up() {
+        $columns = array();
+        $columns[] = Column::create('id')->setType(ColumnType::T_INT)->setIsUnsigned(true)->setNullable(false)->setIsAutoIncrement(true);
+        $columns[] = Column::create('account_id')->setType(ColumnType::T_INT)->setIsUnsigned(true)->setNullable(false);
+        $columns[] = Column::create('level')->setType(ColumnType::T_VARCHAR)->setLength(45);
+        $columns[] = Column::create('region_id')->setType(ColumnType::T_INT)->setIsUnsigned(true)->setNullable(false);
+        $table = TableManager::create('account_profile_contact_place_regions', $columns, 'id');
+        $table->addUnique('id');
+    }
+
+    /** 
+     * (non-PHPdoc)
+     * @see \X\Core\Module\InterfaceMigrate::down()
+     */
+    public function down() {
+        TableManager::open('account_profile_contact_place_regions')->drop();
+    }
+}
