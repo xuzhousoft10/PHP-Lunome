ce6d3230daa2b935e29629a60589152fa2d274ff
michael <michael.the.ranidae@gmail.com>
2014-10-06 19:33:50 +0800

    [module][lunome]add model for table movies

---
+++ Module/Lunome/Model/MovieModel.php
@@ -0,0 +1,33 @@
+<?php
+namespace X\Module\Lunome\Model;
+
+/**
+ * Use statements
+ */
+use X\Service\XDatabase\Core\Table\ColumnType;
+use X\Service\XDatabase\Core\ActiveRecord\Column;
+
+/**
+ * @property string $id
+ * @property string $name
+ **/
+class MovieModel extends \X\Service\XDatabase\Core\ActiveRecord\XActiveRecord {
+    /**
+     * (non-PHPdoc)
+     * @see \X\Service\XDatabase\Core\ActiveRecord\XActiveRecord::describe()
+     */
+    protected function describe() {
+        $columns = array();
+        $columns[] = Column::create('id')->setType(ColumnType::T_VARCHAR)->setLength(32)->setIsPrimaryKey(true)->setNullable(false);
+        $columns[] = Column::create('name')->setType(ColumnType::T_VARCHAR)->setLength(128);
+        return $columns;
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see \X\Service\XDatabase\Core\ActiveRecord\XActiveRecord::getTableName()
+     */
+    protected function getTableName() {
+        return 'movies';
+    }
+}
