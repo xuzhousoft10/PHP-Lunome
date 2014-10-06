2c067059ae48d273301959eb2dfb854236d7557f
michael <michael.the.ranidae@gmail.com>
2014-10-06 22:14:56 +0800

    [module][lunome]Add service Movie

---
+++ Module/Lunome/Service/Movie/Service.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * This file implements the service Movie
+ */
+namespace X\Module\Lunome\Service\Movie;
+
+/**
+ * The service class
+ */
+class Service extends \X\Core\Service\XService {
+    
+}
