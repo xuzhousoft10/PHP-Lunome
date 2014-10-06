35973d9c132d235429370a4cea15aac1eb21f449
michael <michael.the.ranidae@gmail.com>
2014-10-06 22:12:06 +0800

    [module][admin]Add command to generate the service.

--- Module/Admin/Action/Service.php
+++ Module/Admin/Action/Service.php
@@ -1,6 +1,7 @@
 <?php
 namespace X\Module\Admin\Action;
 use X\Core\X;
+use X\Core\Service\Exception;
 use X\Module\Admin\Util\Console;
 /**
  * 
@@ -13,22 +14,45 @@ use X\Module\Admin\Util\Console;
  */
 class Service extends \X\Service\XAction\Core\Action {
     public function runAction( Console $console, $parameters ) {
-        $separatorPos = strpos($parameters, ' ');
-        if ( false !== $separatorPos ) {
-            $subAction = substr($parameters, 0, $separatorPos);
-            $parameters = substr($parameters, $separatorPos+1);
-        } else {
-            $subAction = $parameters;
+        $parameters = explode(' ', $parameters);
+        $actionHandler = 'action';
+        while ( 0 < count($parameters) ) {
+            $actionHandler .= ucfirst(array_shift($parameters));
+            if ( method_exists($this, $actionHandler) ) {
+                break;
+            }
+        }
+        if ( !method_exists($this, $actionHandler) ) {
+            $console->printLine('Command has not been supported.');
+            return;
         }
         
-        $actionHandler = sprintf('action%s', ucfirst($subAction));
-        return call_user_func_array(array($this, $actionHandler), array($console, $parameters));
+        array_unshift($parameters, $console);
+        
+        $method = new \ReflectionMethod(__CLASS__, $actionHandler);
+        $numberOfParameters = $method->getNumberOfParameters();
+        if ( $numberOfParameters !== count($parameters) ) {
+            $message = '%d parameters required, %d were given.';
+            $message = sprintf($message, $numberOfParameters, count($parameters));
+            $console->printLine($message);
+            return;
+        }
+        
+        call_user_func_array(array($this, $actionHandler), $parameters);
     }
     
     protected function actionList( Console $console ) {
         $services = X::system()->getServiceManager()->getList();
         foreach ( $services as $serviceName ) {
             $console->printLine($serviceName);
+        }
+    }
+    
+    protected function actionCreate( Console $console, $name, $module=null) {
+        try {
+            X::system()->getServiceManager()->create($name, $module);
+        } catch ( Exception $e ) {
+            $console->printLine($e->getMessage());
         }
     }
 }
