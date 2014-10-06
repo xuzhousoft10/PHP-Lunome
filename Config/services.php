fa6a26a2b4075029d8c423d0c0aec89aab15d207
michael <michael.the.ranidae@gmail.com>
2014-10-06 19:41:11 +0800

    [config]Update the configuration.

--- Config/services.php
+++ Config/services.php
@@ -1,9 +1,5 @@
 <?php
 return array(
-'XSession'  => array('enable'=>true,'path'=>'Service/XSession/XSession.svc.php'),
-'XError'    => array('enable'=>true,'path'=>'Service/XError/XError.svc.php'),
-'XRequest'  => array('enable'=>false,'path'=>'Service/XRequest/XRequest.svc.php'),
-'XI18n'     => array('enable'=>true,'path'=>'Service/XI18n/XI18n.svc.php'),
 'XDatabase' => array('enable'=>true,'path'=>'Service/XDatabase/XDatabase.svc.php'),
 'XAction'   => array('enable'=>true,'path'=>'Service/XAction/XAction.svc.php'),
 'XView'     => array('enable'=>true,'path'=>'Service/XView/XView.svc.php'),
