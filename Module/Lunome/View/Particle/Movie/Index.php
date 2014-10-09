90019cf0511a1901ed751e0d4156feb1b1215d6a
michael <michael.the.ranidae@gmail.com>
2014-10-09 23:31:32 +0800

    [module][lunome]refine the views.

--- Module/Lunome/View/Particle/Movie/Index.php
+++ Module/Lunome/View/Particle/Movie/Index.php
@@ -41,32 +41,6 @@ $itemMap = array(
   
     <!-- Pager -->
     <div class="panel-footer clearfix">
-        <ul class="pagination pull-right">
-        <li>
-            <a  href    = "/?module=lunome&action=movie/index&mark=<?php echo $pager['params']['mark'];?>" 
-                class   = "<?php if (!$pager['canPrev']):?>disabled<?php endif;?>"
-            >首页</a>
-        </li>
-        <li>
-            <a  href    = "/?module=lunome&action=movie/index&mark=<?php echo $pager['params']['mark'];?>&page=<?php echo $pager['prev'];?>" 
-                class   = "<?php if (!$pager['canPrev']):?>disabled<?php endif;?>"
-            >上一页</a>
-        </li>
-        <?php foreach ($pager['items'] as $pageItem ) : ?>
-            <li <?php if ($pageItem == $pager['current']) :?>class="active"<?php endif;?>>
-                <a href="/?module=lunome&action=movie/index&mark=<?php echo $pager['params']['mark'];?>&page=<?php echo $pageItem;?>"><?php echo $pageItem;?></a>
-            </li>
-        <?php endforeach; ?>
-        <li>
-            <a  href    = "/?module=lunome&action=movie/index&mark=<?php echo $pager['params']['mark'];?>&page=<?php echo $pager['next'];?>" 
-                class   = "<?php if (!$pager['canNext']):?>disabled<?php endif;?>"
-            >下一页</a>
-        </li>
-        <li>
-            <a  href    = "/?module=lunome&action=movie/index&mark=<?php echo $pager['params']['mark'];?>&page=<?php echo $pager['total'];?>" 
-                class   = "<?php if (!$pager['canPrev']):?>disabled<?php endif;?>"
-            >尾页</a>
-        </li>
-        </ul>
+        <?php require $particleViewUtilPath.DIRECTORY_SEPARATOR.'Pager.php'; ?>
     </div>
 </div>
