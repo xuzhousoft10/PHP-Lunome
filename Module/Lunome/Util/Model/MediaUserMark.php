ed998d2d11cb521f774008745e1e3ec1f7dc8a05
michael <michael.the.ranidae@gmail.com>
2014-10-09 22:59:14 +0800

    [module][lunome]delete useless module.

--- Module/Lunome/Util/Model/MediaUserMark.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace X\Module\Lunome\Util\Model;
-use X\Util\Model\Basic;
-abstract class MediaUserMark extends Basic {
-    abstract public function getMediaKey();
-}
--- Module/Lunome/Util/Model/Poster.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace X\Module\Lunome\Util\Model;
-use X\Util\Model\Basic;
-abstract class Poster extends Basic {
-    abstract public function getMediaKey();
-}
