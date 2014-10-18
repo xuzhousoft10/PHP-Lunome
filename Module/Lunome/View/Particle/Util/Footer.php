f8be177a0789c10a3266c9aa71aaf0a571fb1fb8
michael <michael.the.ranidae@gmail.com>
2014-10-18 15:45:26 +0800

    [module][lunome]Remove useless labels on footer

--- Module/Lunome/View/Particle/Util/Footer.php
+++ Module/Lunome/View/Particle/Util/Footer.php
@@ -7,8 +7,8 @@ $this->addStyle('#footer', array('border-top'=>'1px solid #E5E5E5'));
     <P>
         <a href="/index.php?module=lunome&action=advise">意见建议</a> 
         |
-        <a href="/index.php?module=lunome&action=connectus">联系我们</a> 
-  Help | <a href="#">About</a> | <a href="#">@Twitter</a> | <a href="#">Connect Us</a></P>
+        <a href="/index.php?module=lunome&action=connectus">联系我们</a>
+    </P>
 </div>
 <br/><br/>
 <script>
--- Module/Lunome/View/Particle/Util/Navigation.php
+++ Module/Lunome/View/Particle/Util/Navigation.php
@@ -12,43 +12,49 @@ $user = $vars['user'];
     
     <div class="collapse navbar-collapse">
       <ul class="nav navbar-nav navbar-right">
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-            <img src="<?php echo $user['photo']; ?>" class="img-circle" width="20" />
-            <?php echo $user['nickname']; ?> 
-            <span class="caret"></span>
-          </a>
-          <ul class="dropdown-menu">
-            <!-- 
+        <?php if ( $user['isGuest'] ) : ?>
             <li>
-              <a href="/?module=lunome&action=user/information/basic">
-                <span class="glyphicon glyphicon-user"></span>
-                My Profile
-              </a>
+                <a href="/index.php?module=lunome&action=user/login/index">登录</a>
             </li>
-            <li class="divider"></li>
-            <li>
-              <a href="/?module=lunome&action=security/password">
-                <span class="glyphicon glyphicon-heart"></span>
-                Security Center
+        <?php else : ?>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                <img src="<?php echo $user['photo']; ?>" class="img-circle" width="20" />
+                <?php echo $user['nickname']; ?> 
+                <span class="caret"></span>
               </a>
+              <ul class="dropdown-menu">
+                <!-- 
+                <li>
+                  <a href="/?module=lunome&action=user/information/basic">
+                    <span class="glyphicon glyphicon-user"></span>
+                    My Profile
+                  </a>
+                </li>
+                <li class="divider"></li>
+                <li>
+                  <a href="/?module=lunome&action=security/password">
+                    <span class="glyphicon glyphicon-heart"></span>
+                    Security Center
+                  </a>
+                </li>
+                <li>
+                  <a href="/?module=lunome&action=user/setting/basic">
+                    <span class="glyphicon glyphicon-cog"></span>
+                    Personal Setting
+                  </a>
+                </li>
+                <li class="divider"></li>
+                 -->
+                <li>
+                  <a href="/?module=lunome&action=user/logout">
+                    <span class="glyphicon glyphicon-off"></span>
+                    Exit
+                  </a>
+                </li>
+              </ul>
             </li>
-            <li>
-              <a href="/?module=lunome&action=user/setting/basic">
-                <span class="glyphicon glyphicon-cog"></span>
-                Personal Setting
-              </a>
-            </li>
-            <li class="divider"></li>
-             -->
-            <li>
-              <a href="/?module=lunome&action=user/logout">
-                <span class="glyphicon glyphicon-off"></span>
-                Exit
-              </a>
-            </li>
-          </ul>
-        </li>
+        <?php endif; ?>
       </ul>
     </div>
   </div>
