385325ea929bad97bfefa27e3fcdab956bf4cff2
Michael Luthor <michael.the.ranidae@gmail.com>
2014-10-24 10:22:47 +0800

    [module][lunome]Update the index view.

--- Module/Backend/View/Particle/Account/Index.php
+++ Module/Backend/View/Particle/Account/Index.php
@@ -1,14 +1,21 @@
 <?php 
+use X\Module\Lunome\Model\AccountModel;
 /* @var $this X\Service\XView\Core\Handler\Html */
 $vars = get_defined_vars();
+$statusLabels = array(
+    AccountModel::ST_NOT_USED   => '未使用',
+    AccountModel::ST_IN_USE     => '使用中',
+    AccountModel::ST_FREEZE     => '冻结中',
+);
 ?>
 <table class="table table-striped table-hover table-condensed">
     <thead>
         <tr>
             <th>帐号</th>
             <th>昵称</th>
+            <th>Open Id</th>
             <th>状态</th>
-            <th>操作</th>
+            <th>启用期</th>
         </tr>
     </thead>
     <tbody>
@@ -16,8 +23,25 @@ $vars = get_defined_vars();
         <tr>
             <td><?php echo $account['account'];?></td>
             <td><?php echo $account['nickname'];?></td>
-            <td><?php echo $account['status'];?></td>
-            <td></td>
+            <td>
+                <?php if (!empty($account['oauth20_id'])) :?>
+                    <a href="#" class="btn btn-default btn-xs" >查看</a>
+                <?php endif; ?>
+            </td>
+            <td>
+                <?php echo $statusLabels[$account['status']];?>
+                <?php if ( AccountModel::ST_NOT_USED == $account['status'] ) :?>
+                <?php elseif ( AccountModel::ST_IN_USE == $account['status'] ) : ?>
+                    <button type="button" class="btn btn-default btn-xs">冻结</button>
+                <?php elseif ( AccountModel::ST_FREEZE == $account['status'] ) :?>
+                    <button type="button" class="btn btn-default btn-xs">取消冻结</button>
+                <?php endif; ?>
+            </td>
+            <td>
+                <?php if ('0000-00-00 00:00:00' !== $account['enabled_at']): ?>
+                    <?php echo $account['enabled_at'];?>
+                <?php endif;?>
+            </td>
         </tr>
     <?php endforeach; ?>
     </tbody>
