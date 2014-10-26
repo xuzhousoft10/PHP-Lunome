94e23c82e92a23ba229d8eb1ce6cc5928824e9b0
michael <michael.the.ranidae@gmail.com>
2014-10-26 18:02:39 +0800

    [module][backend]add button to back to frontend

--- Module/Backend/View/Layout/Index.php
+++ Module/Backend/View/Layout/Index.php
@@ -7,10 +7,12 @@ $activeMenuItem = $vars['activeMenuItem'];
 <nav class="navbar navbar-default navbar-fixed-top navbar-inverse">
     <div class="container-fluid">
         <div class="navbar-header">
-            <a class="navbar-brand" href="#">Lunome Backend</a>
+            <a class="navbar-brand" href="#">Lunome后台管理</a>
         </div>
-        
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav pull-right">
+                <li><a href="/index.php">回到前台</a></li>
+            </ul>
         </div>
     </div>
 </nav>
