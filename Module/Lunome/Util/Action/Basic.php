c403863f5b882c0c5de18057355ab71b4625e3ac
michael <michael.the.ranidae@gmail.com>
2014-10-26 21:55:16 +0800

    [module][lunome]

--- Module/Lunome/Util/Action/Basic.php
+++ Module/Lunome/Util/Action/Basic.php
@@ -17,17 +17,5 @@ use X\Module\Lunome\Service\User\Service as UserService;
  * @method \X\Module\Lunome\Service\Movie\Service getMovieService()
  */
 abstract class Basic extends \X\Util\Action\Basic {
-    /**
-     * (non-PHPdoc)
-     * @see \X\Service\XAction\Core\Action::beforeRunAction()
-     */
-    protected function beforeRunAction() {
-        $isGuest = $this->getService(UserService::getServiceName())->getIsGuest();
-        if ( $isGuest ) {
-            $this->gotoURL('/index.php?module=lunome&action=user/login/index');
-            X::system()->stop();
-        }
-        
-        parent::beforeRunAction();
-    }
+    
 }
