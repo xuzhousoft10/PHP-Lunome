50b26ef618609f733bd5cee92aed153c1287cab4
Michael Luthor <michael.the.ranidae@gmail.com>
2014-10-30 15:37:24 +0800

    Update comment for core/util/xutil.php

--- Core/Util/XUtil.php
+++ Core/Util/XUtil.php
@@ -1,10 +1,26 @@
 <?php
+/**
+ * 
+ */
 namespace X\Core\Util;
+
+/**
+ * 
+ */
 use X\Core\Basic;
+
+/**
+ * 
+ */
 class XUtil extends Basic {
+    /**
+     * 将php数组保存到指定文件中。
+     * 
+     * @param string $path 目标文件路径。
+     * @param array $data 数组内容。
+     */
     public static function storeArrayToPHPFile( $path, $data ) {
         $var = var_export($data, true);
-        $time = date('Y-m-d H:i:s', time());
         $content = array();
         $content[] = '<?php';
         $content[] = "return $var;";
