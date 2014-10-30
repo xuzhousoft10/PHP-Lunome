2aa11596e652443033c2861d8510705690631bae
Michael Luthor <michael.the.ranidae@gmail.com>
2014-10-30 15:07:50 +0800

    Refined core/basic.php

--- Core/Basic.php
+++ Core/Basic.php
@@ -1,52 +1,53 @@
 <?php
 /**
- * Namespace defination
+ * 
  */
 namespace X\Core;
 
 /**
- * The basic class for the whole framework. 
  * 
- * @author  Michael Luthor <michaelluthor@163.com>
- * @version 0.0.0
- * @since   Version 0.0.0
  */
 class Basic extends \stdClass {
     /**
-     * The magic getter for this class.
-     * 
-     * @param string $name The name of attribute to get.
-     * @throws Exception
+     * 检测属性是否设置，并且不是 NULL。
+     * @param string $name 被检查的属性名称
+     * @return boolean
+     */
+    public function __isset($name){
+        $getter = sprintf('get%s', ucfirst($name));
+        $isset = method_exists($this, $getter) ? (null !== $this->$getter()) : false;
+        return $isset;
+    }
+    
+    /**
+     * 当外部读取一个不存在的属性时， 该方法将会被调用。
+     * @param string $name 要获取的属性名称
      * @return mixed
      */
     public function __get( $name ) {
         $getter = sprintf('get%s', ucfirst($name));
         if ( !method_exists($this, $getter) ) {
-            throw new Exception(sprintf('"%s" does not exists or unable to read.', $name));
+            throw new Exception(sprintf('Property "%s" can not be read.', $name));
         }
-        
-        return $this->$getter();
+        return call_user_func(array($this, $getter));
     }
     
     /**
-     * The magic setter for this class.
-     * 
-     * @param string $name The name of attribute to set.
-     * @param mixed $value The value to set to the attribute.
-     * @throws Exception
+     * 当外部对一个不存在的属性赋值时， 该方法将会被调用。
+     * @param string $name 需要赋值的属性的名称
+     * @param mixed $value 对该属性所赋的值
      * @return void
      */
     public function __set( $name, $value ) {
         $setter = sprintf('set%s', ucfirst($name));
-        if ( method_exists($this, $setter) ) {
-            throw new Exception(sprintf('"%s" does not exists or unable to write.', $name));
+        if ( !method_exists($this, $setter) ) {
+            throw new Exception(sprintf('Property "%s" can not be written.', $name));
         }
-        
-        $this->$setter($value);
+        call_user_func_array(array($this, $setter), array($value));
     }
     
     /**
-     * 
+     * 当需要将类实例转换为字符串时， 该方法将会被调用。
      * @return string
      */
     public function __toString() {
@@ -54,10 +55,10 @@ class Basic extends \stdClass {
     }
     
     /**
-     * 
+     * 获取该类的字符串表现形式。默认情况下返回类名， 一般需要重写该方法。
      * @return string
      */
     public function toString() {
-        return __CLASS__;
+        return get_class($this);
     }
 }
