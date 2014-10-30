aff8908be72c11249cb26ed587443446d2d8abb5
Michael Luthor <michael.the.ranidae@gmail.com>
2014-10-30 14:33:02 +0800

    Update the comments.

--- Core/Exception.php
+++ Core/Exception.php
@@ -1,11 +1,10 @@
 <?php
+/**
+ * 
+ */
 namespace X\Core;
 
 /**
- * The exception class for X core classes.
  * 
- * @author  Michael Luthor <michaelluthor@163.com>
- * @since   Version 0.0.0
- * @version 0.0.0
  */
 class Exception extends \Exception {}
