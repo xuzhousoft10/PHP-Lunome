3dca6b5a2d71eae5f8d61401d161ff43d24e6fa3
michael <michael.the.ranidae@gmail.com>
2014-10-30 21:06:04 +0800

    update comment for core/service/exception.php

--- Core/Service/Exception.php
+++ Core/Service/Exception.php
@@ -5,10 +5,6 @@
 namespace X\Core\Service;
 
 /**
- * The exception class for XService classes.
  * 
- * @author  Michael Luthor <michaelluthor@163.com>
- * @since   Version 0.0.0
- * @version 0.0.0
  */
 class Exception extends \Exception {}
