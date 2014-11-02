b3a6302cac933b454e6c064d78dc527b3cc9aa1f
michael <michael.the.ranidae@gmail.com>
2014-11-02 19:23:43 +0800

    updated the comments of core/module/exception.php

--- Core/Module/Exception.php
+++ Core/Module/Exception.php
@@ -1,10 +1,10 @@
 <?php
+/**
+ * 
+ */
 namespace X\Core\Module;
 
 /**
- * The exception class for Module classes.
  * 
- * @author  Michael Luthor <michaelluthor@163.com>
- * @since   Version 0.0.0
  */
 class Exception extends \Exception {}
