bddfa868c4910dcd223468711f8747f0a27acd1e
michael <michael.the.ranidae@gmail.com>
2014-11-21 23:43:29 +0800

    Fixed that qq service can not under cli mode

--- Service/QQ/Config/Main.php
+++ Service/QQ/Config/Main.php
@@ -3,6 +3,6 @@ return array(
 'QQConnect' => array(
     'appid'     => '101161224',
     'appkey'    => 'f03143e996578b9222180fc85d594473',
-    'callback'  => sprintf('http://%s/index.php?module=lunome&action=user/login/qqcallback', $_SERVER['HTTP_HOST']),
+    'callback'  => sprintf('http://%s/index.php?module=lunome&action=user/login/qqcallback', isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : ''),
 )
 );
