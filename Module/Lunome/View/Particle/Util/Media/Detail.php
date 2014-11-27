2c68f6feb9ddd9b442e2e44e892f7a22908139ff
michael <michael.the.ranidae@gmail.com>
2014-11-27 23:20:18 +0800

    [Done #32]Goto video search page when user clicked the cover on detail page.

--- Module/Lunome/View/Particle/Util/Media/Detail.php
+++ Module/Lunome/View/Particle/Util/Media/Detail.php
@@ -28,7 +28,7 @@ if ( MovieService::MARK_INTERESTED == $myMark ) {
     </ol>
     
     <div class="col-md-2 padding-0">
-        <a href="http://www.baidu.com/s?wd=<?php echo urlencode($media['name']); ?>" target="_black">
+        <a href="http://v.baidu.com/v?word=<?php echo urlencode(mb_convert_encoding($media['name'],'gb2312','utf-8' )); ?>" target="_black">
             <img src="<?php echo $media['cover'];?>" width="200" height="300">
         </a>
     </div>
