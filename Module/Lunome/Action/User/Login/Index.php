926ad751e9244098971de9b800519f3b156f4634
michael <michael.the.ranidae@gmail.com>
2014-12-14 18:49:17 +0800

    [Close #67]{Remove} Disabled login by sina weibo.

--- Module/Lunome/Action/User/Login/Index.php
+++ Module/Lunome/Action/User/Login/Index.php
@@ -9,7 +9,6 @@ namespace X\Module\Lunome\Action\User\Login;
  */
 use X\Core\X;
 use X\Module\Lunome\Util\Action\Visual;
-use X\Module\Lunome\Service\Configuration\Service as ConfigService;
 
 /**
  * The action class for account/login action.
@@ -21,19 +20,6 @@ class Index extends Visual {
      * @return void
      */ 
     public function runAction( ) {
-        $this->getView()->title = "登录 | Lunome";
-        $this->getView()->loadLayout($this->getLayoutViewPath('Blank'));
-        $this->getView()->removeParticle('INDEX_NAV_BAR');
-        /* @var $configService ConfigService */
-        $configService = X::system()->getServiceManager()->get(ConfigService::getServiceName());
-        
-        $name   = 'USER_LOGIN';
-        $path   = $this->getParticleViewPath('User/Login');
-        $option = array();
-        $data   = array(
-            'backgroundImage'  => $configService->get('login_background_image'),
-            'QQLoginIcon'     => $configService->get('qq_login_icon'), 
-        );
-        $this->getView()->loadParticle($name, $path, $option, $data);
+        $this->gotoURL('/index.php?module=lunome&action=user/login/qq');
     }
 }
