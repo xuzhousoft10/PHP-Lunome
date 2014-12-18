7c872bd0f1c2c9c9d77d60e72ffcf96c7c2d049b
michael <michael.the.ranidae@gmail.com>
2014-12-19 00:39:23 +0800

    Add comment to xlog config file.

--- Service/XLog/Config/Main.php
+++ Service/XLog/Config/Main.php
@@ -16,6 +16,26 @@ return array(
                 'user'          => 'root',
                 'password'      => '',
                 'table'         => 'system_log_debug',
+                /**
+                    CREATE TABLE `system_log_debug` (
+                      `time` datetime DEFAULT NULL,
+                      `logger` varchar(64) DEFAULT NULL,
+                      `level` varchar(16) DEFAULT NULL,
+                      `message` varchar(256) DEFAULT NULL,
+                      `exception` text,
+                      `file` varchar(1024) DEFAULT NULL,
+                      `line` int(11) DEFAULT NULL,
+                      `class` varchar(256) DEFAULT NULL,
+                      `method` varchar(64) DEFAULT NULL,
+                      `sid` varchar(64) DEFAULT NULL,
+                      `session` text,
+                      `pid` varchar(16) DEFAULT NULL,
+                      `env` text,
+                      `server` text,
+                      `request` text,
+                      `cookie` text
+                    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+                 */
                 'insertSql'     => 'INSERT INTO system_log_debug ('.
                                    'time,logger,level,message,exception,file,line,class,method,sid,session,pid,env,server,request,cookie'.
                                    ') VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',
