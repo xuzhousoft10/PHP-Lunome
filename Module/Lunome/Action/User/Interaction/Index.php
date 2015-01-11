8fb682beede90f25ce5ebc010a64f3fb39f90634
michael <michael.the.ranidae@gmail.com>
2015-01-11 13:28:14 +0800

    Prepare for Interaction

---
+++ Module/Lunome/Action/User/Interaction/Index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * 
+ */
+namespace X\Module\Lunome\Action\User\Interaction;
+
+/**
+ * 
+ */
+use X\Module\Lunome\Util\Action\Visual;
+
+/**
+ * 
+ */
+class Index extends Visual {
+    /**
+     * @param unknown $id
+     */
+    public function runAction( $id ) {
+        echo "OK";
+    }
+}
