1df44475e9cf05d683bef7d1ac48f675595eea8c
Michael Luthor <michael.the.ranidae@gmail.com>
2015-02-04 15:47:44 +0800

    use local as assets container.

--- Config/main.php
+++ Config/main.php
@@ -1,7 +1,5 @@
 <?php
 return array (
     'is_debug'         => true,
-    'assets-base-url' => (isset($_SERVER['HTTP_HOST']) && 'lunome.kupoy.com' === $_SERVER['HTTP_HOST']) 
-                        ? 'http://lunome.kupoy.com/Assets'
-                        : 'http://lunome-assets.qiniudn.com',
+    'assets-base-url' => 'http://lunome.kupoy.com/Assets',
 );
