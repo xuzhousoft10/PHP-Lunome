928aaffe317a40617c697bf2ce8179d33ea6de07
michael <michael.the.ranidae@gmail.com>
2015-02-06 23:39:09 +0800

    [backend]Update movie detail page.

--- Module/Backend/Action/Movie/Detail.php
+++ Module/Backend/Action/Movie/Detail.php
@@ -7,7 +7,9 @@ namespace X\Module\Backend\Action\Movie;
 /**
  * 
  */
+use X\Core\X;
 use X\Module\Backend\Util\Action\Visual;
+use X\Module\Lunome\Service\Region\Service as RegionService;
 
 /**
  * 
@@ -19,16 +21,30 @@ class Detail extends Visual {
     public function runAction($id) {
         $view = $this->getView();
         $movieService = $this->getMovieService();
+        /* @var $regionService RegionService */
+        $regionService = X::system()->getServiceManager()->get(RegionService::getServiceName());
+        
         if ( !$movieService->has($id) ) {
             $this->throw404();
         }
         
         $movie = $movieService->get($id);
+        if ( 1 === (int)$movie['has_cover'] ) {
+            $movie['cover'] = $movieService->getCoverURL($movie['id']);
+        } else {
+            $movie['cover'] = $movieService->getMediaDefaultCoverURL();
+        }
+        
+        $movie['language'] = $movieService->getLanguageById($movie['language_id'])->name;
+        $movie['region'] = $movieService->getRegionById($movie['region_id'])->name;
+        $movie['length'] = ((int)($movie['length']/3600)).'小时'.((int)($movie['length']%60)).'分钟';
         
         $viewName = 'BACKEND_MVOIE_DETAIL';
         $viewPath = $this->getParticleViewPath('Movie/Detail');
         $view->loadParticle($viewName, $viewPath);
         $view->setDataToParticle($viewName, 'movie', $movie);
+        $view->setDataToParticle($viewName, 'actors', $movieService->getActors($movie['id']));
+        
         
         $this->setPageTitle($movie['name']);
         $this->setMenuItemActived(self::MENU_ITEM_MOVIE);
--- Module/Backend/View/Particle/Movie/Detail.php
+++ Module/Backend/View/Particle/Movie/Detail.php
@@ -1,7 +1,45 @@
 <?php 
 $vars = get_defined_vars();
 $movie = $vars['movie'];
+$actors = $vars['actors'];
 ?>
-<?php 
-var_dump($movie);
-?>
+<div class="clearfix">
+    <div class="col-md-10 padding-0">
+        <table class="table table-striped table-bordered table-hover">
+            <tr><th>名称</th><th><?php echo $movie['name'];?></th></tr>
+            <tr><th>长度</th><th><?php echo $movie['length'];?></th></tr>
+            <tr><th>日期</th><th><?php echo $movie['date'];?></th></tr>
+            <tr><th>区域</th><th><?php echo $movie['region'];?></th></tr>
+            <tr><th>语言</th><th><?php echo $movie['language'];?></th></tr>
+        </table>
+        <br>
+        <a class="btn" href="/?module=backend&action=movie/edit&id=<?php echo $movie['id'];?>">编辑</a>
+    </div>
+    <div class="col-md-2">
+        <img src="<?php echo $movie['cover'];?>" class="img-thumbnail padding-0">
+    </div>
+</div>
+<br>
+演员
+<hr>
+<div>
+<?php foreach ( $actors as $actor ) : ?>
+<div class="btn-group">
+    <button type="button" class="btn btn-default"><?php echo $actor->name;?></button>
+    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+        <span class="caret"></span>
+    </button>
+    <ul class="dropdown-menu">
+        <li class="divider"></li>
+        <li><a href="/?module=backend&action=movie/deleteActor&movie=<?php echo $movie['id'];?>&actor=<?php echo $actor->id; ?>">删除</a></li>
+     </ul>
+</div>
+<?php endforeach; ?>
+<br><br>
+<form class="form-inline">
+    <div class="form-group">
+        <input type="text" class="form-control">
+    </div>
+    <button type="submit" class="btn btn-default">增加</button>
+</form>
+</div>
