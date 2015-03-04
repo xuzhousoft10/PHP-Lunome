cb3ffddeabfde315b404c856009397cdfcbf64b4
michael <michael.the.ranidae@gmail.com>
2015-03-04 20:33:35 +0800

    Update Util/Model/Basic

--- Util/Model/Basic.php
+++ Util/Model/Basic.php
@@ -13,16 +13,27 @@ use X\Service\XDatabase\Core\ActiveRecord\XActiveRecord;
  * The basic model class
  */
 abstract class Basic extends XActiveRecord {
-    protected function afterInit() {
+    /**
+     * (non-PHPdoc)
+     * @see \X\Service\XDatabase\Core\ActiveRecord\XActiveRecord::init()
+     */
+    protected function init() {
+        parent::init();
         if ( $this->has('id') ) {
             $this->getAttribute('id')->setValueBuilder(array($this, 'buildId'));
         }
     }
     
+    /**
+     * @param unknown $record
+     */
     public function buildId( $record ) {
         return$this->generateUUID();
     }
     
+    /**
+     * @return string
+     */
     protected function generateUUID () {
         return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
                 mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
