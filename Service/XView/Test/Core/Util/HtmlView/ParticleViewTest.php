e2b7e2bde0f4771a46bab4afff47cbba422b7544
michael <michael.the.ranidae@gmail.com>
2015-03-06 22:37:36 +0800

    Update test case for service XView

--- Service/XView/Test/Core/Util/HtmlView/ParticleViewTest.php
+++ Service/XView/Test/Core/Util/HtmlView/ParticleViewTest.php
@@ -26,6 +26,33 @@ class ParticleViewTest extends TestCase {
         ob_implicit_flush(false);
         $particleView->display();
         $viewContent = ob_get_clean();
-        $this->assertSame("v1:value1\npv1:value1", $viewContent);
+        $this->assertSame("v1:value1\npv1:value1\n", $viewContent);
+        $particleView->cleanUp();
+        
+        $particleView = $html->getParticleViewManager()->load('test2', array($this, 'particleViewHandler'));
+        $particleView->getOptionManager()->set('mode', '1');
+        $this->assertSame('value1-1', $particleView->toString());
+        $particleView->getOptionManager()->set('mode', '2');
+        $particleView->cleanUp();
+        $this->assertSame('value1-X', $particleView->toString());
+        
+        $particleView = $html->getParticleViewManager()->load('test2', 'JUST STRING');
+        $this->assertSame('JUST STRING', $particleView->toString());
+        
+        $particleView = $html->getParticleViewManager()->load('test2', null);
+        $this->assertSame(null, $particleView->toString());
+    }
+     
+    /**
+     * @param unknown $data
+     * @param unknown $option
+     * @return string
+     */
+    public function particleViewHandler( $data, $option ) {
+        if ( '1' === $option['mode'] ) {
+            return $data['v1'].'-1';
+        } else {
+            return $data['v1'].'-X';
+        }
     }
 }
